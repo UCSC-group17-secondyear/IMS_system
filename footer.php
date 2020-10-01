45069c6840c42d4744c6687c605b0aa50cbd02e1
malithya_fernando <malithyafernando@gmail.com>
2020-10-01 12:01:42 +0530

    MVC update

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-	<div class="footerStyle">
-		<!-- <h5>© 2020, All rights reserved by University of Colombo School of Computing
-			<p>No: 35, Reid Avenue, Colombo 7, Sri Lanka.</p></h5> -->
-	</div>
-</footer>
-</body>
-</html>
