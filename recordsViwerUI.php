f1594ba1413f56fba2122b8dc76d5ae87d269b53
chathuRashmini <chathu.rash97@gmail.com>
2020-10-02 09:18:44 +0530

    Uploaded records viewer's ui flow.

---
+++ recordsViwerUI.php
@@ -0,0 +1,36 @@
+<?php
+    require "header.php";
+?>
+
+<main>
+    <link rel="stylesheet" href="style.css">
+    <!-- <div class="titleStyle"><h2>Welcome to the Admin's UI </h2></div> -->
+    <ul class="breadcrumb">
+        <li><a href="homePage.php">Home Page</a></li>
+        <li>Records Viewer Page</li>
+    </ul>
+
+    <div class="sidenav">
+    <!-- <form action="adminPage.php" method="post">	 -->
+        <a href="amViewStudentsAttendanceV.php">View Students attendance Reports</a>
+        <br>
+        <a href="mmViewMahapolaStudentListV.php">View Mahapola Nominated Student List</a>
+        <br>
+        <a href="mmViewMahapolaReportsV.php">View Reports of Mahapola Scheme</a>
+        <br>
+        <a href="msmViewMedicalMemberListV.php">View Memeber List of Medical Scheme</a>
+        <br>
+        <a href="msmViewMedicalSchemeReportsV.php">View Forms of Medical Scheme</a>
+        <br>
+        <a href="msmViewClaimDetailsV.php">View Claim Details</a>
+        <br>
+        <a href="uMedicalSchemeRegistrationV.php">Register to the Medical Scheme</a>
+        <br>
+        <a href="uMedicalSchemeDetailsV.php">View Medical Scheme Details</a>
+        <br>
+    </div>
+</main>
+<!-- 
+<?php
+    require "footer.php";
+?> -->
