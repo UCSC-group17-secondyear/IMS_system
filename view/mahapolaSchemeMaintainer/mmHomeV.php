304811f12647582e54408ed1a1516c8a55de375f
Janith Renuka <56946043+janithrenuka@users.noreply.github.com>
2020-10-05 21:48:18 +0530

    Update UI profile

--- view/mahapolaSchemeMaintainer/mmHomeV.php
+++ view/mahapolaSchemeMaintainer/mmHomeV.php
@@ -8,20 +8,19 @@
 </head>
 <body>
       <div class="container">
-        <div class="header">
+        <div class="header breadcrumb">
               <!-- <div class="nameLogo"> -->
             <img src="../img/ims.jpg" alt="ims" class="logo">
             <!-- </div> -->
             <div class="options">
                 <a href="mmHomeV.php">Home</a>
+                <a href="mmProfileV.php">Profile</a>
                 <a href="#">Logout</a>
             </div>
         </div>
         <div class="header">breadcrums</div>
         <div class="side-nav">
-            <div>
-                  <h2>Mahapola Scheme Maintainer</h2>
-            </div>
+            
             
                   <a href="mmMarkMahapolaSelectedStudentsV.php" ><button type="submit" name="" class="button">Mark Mahapola Selected Students</button></a><br>
                   <a href="mmViewMahapolaNominatedListV.php" ><button type="submit" name="" class="button">View Mahapola Nominated Student List</button></a><br>
@@ -32,9 +31,17 @@
                   <a href="mmRegisterToMedicalSchemeV.php" ><button type="submit" name="" class="button">Register to Staff Medical Scheme</button></a><br>
             
         </div>
-        <div class="banner">Banner</div>
+        <div class="banner">
+            <div>
+                  <h2>Mahapola Scheme Maintainer</h2>
+            </div>
+        </div>
         <div class="content">Content</div>
-        <div class="footer">Footer</div>
+        <div class="footer">
+            <?php
+              require_once('../include/footer.php');
+            ?>
+        </div>
       </div>
 
       
