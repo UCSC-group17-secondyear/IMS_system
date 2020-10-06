9bc9779bbfec8ce26db057a12bad4cb60ba6e73e
chathuRashmini <chathu.rash97@gmail.com>
2020-10-06 20:49:22 +0530

    Added side navigation to the Hall alloation maintainer's views

--- view/hallAllocationMaintainer/hamRegisterToMedicalSchemeV.php
+++ view/hallAllocationMaintainer/hamRegisterToMedicalSchemeV.php
@@ -18,25 +18,19 @@
                 <a href="#">Logout</a>
             </div>
         </div>
-        <div class="header">breadcrums</div>
-        <div class="side-nav">
-           
 
-        <!-- <div> -->
-                <a href="hamWeeklyTimeTableV.php"><button type="submit" name="" class="button">View Weekly Time Table</button></a><br>
-                <a href="hamViewHallAllocationScheduleV.php"><button type="submit" name="" class="button">View Hall Allocation Schedule</button></a><br>
-                <a href="hamViewSchemeDetailsV.php"><button type="submit" name="" class="button">View Scheme Details</button></a><br>
-                <a href="hamHallDetailsV.php"><button type="submit" name="" class="button">View Hall Details</button></a><br>
-                <a href="hamManageWeeklyTimeTableV.php"><button type="submit" name="" class="button">Manage Weekly Time Table</button></a><br>
-                <a href="hamManageBookingV.php"><button type="submit" name="" class="button">Manage Booking</button></a><br>
-                <a href="hamRegisterToMedicalSchemeV.php"><button type="submit" name="" class="button">Register to the Staff Medical Scheme</button></a><br>
-            <!-- </div> -->
-        </div>
+        <div class="header">breadcrums</div>
+        
+        <?php
+            require_once('hamSideNavV.php');
+        ?>
+
         <div class="banner">
             <div>
                 <h2>Hall Allocation Maintainer</h2>
             </div>
         </div>
+        
         <div class="content">
             <form action="" method="post">
                 <label for="empName">Employee Name</label>
@@ -68,10 +62,8 @@
                 </select> <br>
             </form>
             <a href="hamRegisterSuccessV.php"><button type="submit" name="registerMedicalScheme-submit">Register</button></a><br>
-    
-
-            
         </div>
+        
         <div class="footer">
             <?php
                 require_once('../include/footer.php');
---
+++ view/hallAllocationMaintainer/hamSideNavV.php
@@ -0,0 +1,11 @@
+<main>
+    <div class="side-nav">
+        <a href="hamWeeklyTimeTableV.php"><button type="submit" name="" class="button">View Weekly Time Table</button></a><br>
+        <a href="hamViewHallAllocationScheduleV.php"><button type="submit" name="" class="button">View Hall Allocation Schedule</button></a><br>
+        <a href="hamViewSchemeDetailsV.php"><button type="submit" name="" class="button">View Scheme Details</button></a><br>
+        <a href="hamHallDetailsV.php"><button type="submit" name="" class="button">View Hall Details</button></a><br>
+        <a href="hamManageWeeklyTimeTableV.php"><button type="submit" name="" class="button">Manage Weekly Time Table</button></a><br>
+        <a href="hamManageBookingV.php"><button type="submit" name="" class="button">Manage Booking</button></a><br>
+        <a href="hamRegisterToMedicalSchemeV.php"><button type="submit" name="" class="button">Register to the Staff Medical Scheme</button></a><br>
+    </div>
+</main>
