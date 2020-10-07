e0617e9fc6603f548e47e8018a28f731f246bd8c
chathuRashmini <chathu.rash97@gmail.com>
2020-10-07 11:35:18 +0530

    Added side navigation files for two user roles

--- view/medicalSchemeMember/memClaimDetailsFormListV.php
+++ view/medicalSchemeMember/memClaimDetailsFormListV.php
@@ -20,16 +20,11 @@
             </div>
         </div>
         <div class="header">breadcrums</div>
-        <div class="side-nav">
-            
-            
-                  <a href="memRenewMembershipV.php" ><button type="submit" name="" class="button">Renew Membership</button></a><br>
-                  <a href="memViewSchemeDetailsV.php" ><button type="submit" name="" class="button">View Medical Scheme Details</button></a><br>
-                  <a href="memFillClaimFormsV.php" ><button type="submit" name="" class="button">Fill Claim Forms</button></a><br>
-                  <a href="memUpdateClaimFormsV.php" ><button type="submit" name="" class="button">Update Claim Form</button></a><br>
-                  <a href="memViewClaimFormsV.php" ><button type="submit" name="" class="button">View Claim Forms</button></a><br>
-                  <a href="memGetClaimDetailsV.php" ><button type="submit" name="" class="button">Get Claim Detials</button></a><br>
-        </div>
+        
+        <?php
+          require_once('memSideNavV.php');
+        ?>
+
         <div class="banner">
             <div>
                   <h2>Medical Scheme Member</h2>
