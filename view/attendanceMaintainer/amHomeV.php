f66da7665c69133e4f656f5035d47a0087607242
chathuRashmini <chathu.rash97@gmail.com>
2020-10-07 14:31:38 +0530

    Updated Attendance maintainer's ui flow

--- view/attendanceMaintainer/amHomeV.php
+++ view/attendanceMaintainer/amHomeV.php
@@ -1,23 +1,14 @@
-<html>
-<head>
-<title>Admin's Page</title>
-<link rel="stylesheet" href="../assests/css/style.css">
-</head>
-<body>
-    <?php
-        require "../header.php";
-    ?>
-    <div class="container">
-        <ul class="breadcrumb">
-            <li><a href="homePageV.php">Home</a></li>
-            <li>Attendance Maintainer's Page</li>
-        </ul>
-    </div>
-    <?php
-        require "amSideNavV.php";
-    ?>
-    <?php
-        require "../footer.php";
-    ?>
-</body>
-</html>
+<?php
+    require "../header.php";
+    require "amSideNavV.php";
+?>
+<main>
+    <link rel="stylesheet" href="../assests/css/style.css">
+
+<div class="contnet">
+    
+</div>
+
+<?php
+    require "../footer.php";
+?>
--- view/attendanceMaintainer/amMonthWiseAttendanceV.php
+++ view/attendanceMaintainer/amMonthWiseAttendanceV.php
@@ -1,35 +1,35 @@
-<html>
-    <head>
-        <title>Attendance Maintainer Page</title>
-        <link rel="stylesheet" href="../assests/css/style.css">
-    </head>
-    <body>
-        <?php
-            require "../header.php";
-        ?>
+<?php
+    require "../header.php";
+    require "amSideNavV.php";
+?>
+
+<main>
+    <link rel="stylesheet" href="../assests/css/style.css">
+    
+    <div class="container">
         <ul class="breadcrumb">
-            <li><a href="homePageV.php">Home</a></li>
-            <li><a href="amHomeV.php">Attendance Maintainer Page</a></li>
-            <li>View Attendance - Student Wise</li>
+            <li><a href="amHomeV.php">Home</a></li>
+            <li>View Month Wise Attendance</li>
         </ul>
-        <?php
-            require "amSideNavV.php";
-        ?>
 
-        <div class="formStyle">
-        <form action="amMonthWiseAttendanceV.php" method="post">
-            <input type="number" name="calander_year" placeholder="Calander Year" required>
-            <input type="text" name="month" placeholder="Month" required>
-            <input type="text" name="degree" placeholder="Degree" required>
-            <input type="text" name="academic_year" placeholder="Academic Year" required>
-            <input type="text" name="semester" placeholder="Semester" required>
-            <input type="text" name="subject" placeholder="Subject" required>
-            <button type="submit" name="select-submit" href="amDisplayAttendanceV.php">Display Attendance</button>
-        </form>
+        <div class="content">
+            <div>
+                <h3>Month Wise Attendance</h3>
+            </div>
+
+            <form action="amMonthWiseAttendanceV.php" method="post">
+                <input type="number" name="calander_year" placeholder="Calander Year" required/>
+                <input type="text" name="month" placeholder="Month" required/>
+                <input type="text" name="degree" placeholder="Degree" required/>
+                <input type="text" name="academic_year" placeholder="Academic Year" required/>
+                <input type="text" name="semester" placeholder="Semester" required/>
+                <input type="text" name="subject" placeholder="Subject" required/>
+                <button type="submit" name="select-submit" href="amDisplayAttendanceV.php">Display Attendance</button>
+            </form>
+        </div>
     </div>
+</main>
 
-        <?php
-            require "../footer.php";
-        ?>
-    </body>
-</html>
+<?php
+    require "../footer.php";
+?>
