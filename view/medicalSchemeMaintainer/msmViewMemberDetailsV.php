ebf6fca9e3151a007103c5e555313f2cd9b84f11
milanmalshika <milanmalshika22@gmail.com>
2020-10-07 20:22:55 +0530

    update ui Flows

--- view/medicalSchemeMaintainer/msmViewMemberDetailsV.php
+++ view/medicalSchemeMaintainer/msmViewMemberDetailsV.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View / Delete Member</title>
+    <title>View Member Details</title>
     <link rel="stylesheet" href="../css/main.css">
 </head>
+
 <body>
     <div class="container">
         <div class="header">
@@ -18,39 +20,29 @@
                 <a href="#">Logout</a>
             </div>
         </div>
+
         <div class="header">breadcrums</div>
-        <div class="side-nav"> 
-            
 
-            <!-- <div> -->
-                <a href="msmViewMedicalMemberListV.php"><button type="submit" name="" class="button">View Medical Member List</button></a><br>
-                <a href="msmRemoveMemberV.php"><button type="submit" name="" class="button">Remove Member</button></a><br>
-                <a href="msmViewClaimDetailsV.php"><button type="submit" name="" class="button">View Claim Details</button></a><br>
-                <a href="msmViewFormsV.php"><button type="submit" name="" class="button">View Forms of the Medical Scheme</button></a><br>
-                <a href="msmViewSchemeDetailsV.php"><button type="submit" name="" class="button">View Medical Scheme Details</button></a><br>
-                <a href="msmRegisterToMedicalSchemeV.php"><button type="submit" name="" class="button">Register to the Staff Medical Scheme</button></a><br>
-            <!-- </div> -->
-        </div>
-        <div class="banner">
-            <div>
-                <h2>Medical Scheme Maintainer</h2>
-            </div>
-        </div>
+        <?php
+            require_once('msmSideNavV.php');
+        ?>
+
         <div class="content">
-                    <div>
-                    <h3>Member Details</h3>
-                </div>
+            <div>
+                <h3>Member Details</h3>
+            </div>
 
-                <!-- get method wge mokk hri ekakin id eken details aran display krnna ona -->
+            <!-- get method wge mokk hri ekakin id eken details aran display krnna ona -->
 
-                <a href="msmRemoveMemberV.php"><button onclick="myFunction()">Delete</button></a>
+            <a href="msmRemoveMemberV.php"><button onclick="myFunction()">Delete</button></a>
 
-                <script>
-                    function myFunction(){
-                        alert("The employee has been removed successfully.");
-                    }
-                </script>
+            <script>
+            function myFunction() {
+                alert("The employee has been removed successfully.");
+            }
+            </script>
         </div>
+
         <div class="footer">
             <?php
                 require_once('../include/footer.php');
@@ -58,4 +50,5 @@
         </div>
     </div>
 </body>
+
 </html>
