2a5d4d78fa7ae9810d3651f27b3ae7fd9c8d6e0c
milanmalshika <milanmalshika22@gmail.com>
2020-10-08 13:38:06 +0530

    update ui flow

--- view/homePageV.php
+++ view/homePageV.php
@@ -1,26 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-	    <meta charset="UTF-8">
-	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	    <title>Home Page</title>
-	    <link rel="stylesheet" href="../css/main.css">
-	</head>
-<body>
+<main>
 	<?php
 		require "header.php";
 	?>
-
-	<main>
-		<div class="container">
-			<div class="banner">
-				<h2>IMS of Academic And Publication Division </h2>
-			</div>
+	<div class="header"></div>
+	<div class="side-nav">
+		
+	</div>
+	<div class="banner">
+		<div style="text-align: center;">
+			<h2>IMS of Academic And Publication Division </h2>
 		</div>
-	</main>
+	</div>
 
-	<?php
-		require "footer.php";
-	?>
-</body>
-</html>
+		<?php
+			require 'footer.php';
+		?>
+</main>
+
+
