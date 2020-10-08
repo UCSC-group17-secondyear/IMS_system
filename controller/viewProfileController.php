6766f6d5e100067f68bbd35e325b1445abcdbd10
milanmalshika <milanmalshika22@gmail.com>
2020-10-08 18:18:53 +0530

    update controllers and model

---
+++ controller/viewProfileController.php
@@ -0,0 +1,69 @@
+<?php
+
+    session_start();
+    require_once('../model/Model.php');
+    require_once('../config/database.php');
+
+?>
+
+<?php
+ 
+    $user_id = mysqli_real_escape_string($connect, $_GET['user_id']);
+    // echo $user_id;
+    $result_set = Model::view($user_id, $connect);
+
+    if ($result_set) {
+        if(mysqli_num_rows($result_set)==1){
+            $result = mysqli_fetch_assoc($result_set);
+            $_SESSION['userId'] = $result['userId'];
+            $_SESSION['empid'] = $result['empid'];
+            $_SESSION['initials'] = $result['initials'];
+            $_SESSION['sname'] = $result['sname'];
+            $_SESSION['email'] = $result['email'];
+            $_SESSION['mobile'] = $result['mobile'];
+            $_SESSION['tp'] = $result['tp'];
+            $_SESSION['dob'] = $result['dob'];
+            $_SESSION['designation'] = $result['designation'];
+            $_SESSION['appointment'] = $result['appointment'];
+
+            if ($result['userRole'] == "admin") {
+                header('Location:../view/admin/aProfileV.php');
+            }
+            else if ($result['userRole'] == "academicStaffMemb") {
+                header('Location:../view/academicStaffMember/asmProfileV.php');
+            }
+            else if ($result['userRole'] == "attendanceMain") {
+                header('Location:../view/attendanceMaintainer/amProfileV.php');
+            }
+            else if ($result['userRole'] == "hallAllocationMain") {
+                header('Location:../view/hallAllocationMaintainer/hamProfileV.php');
+            }
+            else if ($result['userRole'] == "mahapolaSchemeMain") {
+                header('Location:../view/mahapolaSchemeMaintainer/mmProfileV.php');
+            }
+            else if ($result['userRole'] == "medicalSchemeMain") {
+                header('Location:../view/medicalSchemeMaintainer/msmProfileV.php');
+            }
+            else if ($result['userRole'] == "recordsViewer") {
+                header('Location:../view/recordsViewer/recordsViwerProfileUI.php');
+            }
+            else if ($result['userRole'] == "departmentHead") {
+                header('Location:../view/departmentHead/dhProfileV.php');
+            }
+            else if ($result['userRole'] == "medicalOfficer") {
+                header('Location:../view/medicalOfficer/medicalOfficerProfileV.php');
+            }
+            else {
+                echo "USER";
+            }
+
+        }
+        else{
+            echo "User not Found!";
+        }
+    }
+    else{
+        echo "Query Unsuccessful";
+    }
+
+?>
