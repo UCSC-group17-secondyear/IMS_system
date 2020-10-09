7b8370653b196f65f3572583f795d0ead35d5d90
Janith Renuka <56946043+janithrenuka@users.noreply.github.com>
2020-10-09 07:39:03 +0530

    Update mm UI

--- view/mahapolaSchemeMaintainer/mmMarkMahapolaStudentsIndexV.php
+++ view/mahapolaSchemeMaintainer/mmMarkMahapolaStudentsIndexV.php
@@ -1,61 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mark Mahapola Selected Students By Index</title>
-    <link rel="stylesheet" href="../css/main.css"></link>
+<main>
 
-</head>
+        <title>Mark Mahapola Slected Students</title>
 
-<body>
-    <div class="container">
-        <div class="header">
-              <!-- <div class="nameLogo"> -->
-              <img src="../img/ims.jpg" alt="ims" class="logo">
-            <!-- </div> -->
-            <div class="options">
-                <a href="mmHomeV.php">Home</a>
-                <a href="mmProfileV.php">Profile</a>
-                <a href="#">Logout</a>
-            </div>
-        </div>
-        <!-- breadcrumbs -->
-        <ul class="breadcrumb">
-            <li><a href="mmHomeV.php">Home</a></li>
-            <li>Mark Mahapola Selected Students By Index</li>
-        </ul>
+<?php
+    require('../basic/header.php');
+    
+?>
+
         
-        <?php
-          require_once('mmSideNavV.php');
-        ?>
-        
-        <div class="banner">
-            <div>
-                  <h2>Mahapola Scheme Maintainer</h2>
-            </div>
-        </div>
-        <div class="content">
-            <div>
-                <h4>Mark Mahapola Selected Students</h4>
-            </div>
+            <div class="header">Breadcrumbs</div>
 
-            <form action="" method="POST">
-
-                <!-- <input type="radio" id="studentindex" name="student" value="studentindex"> -->
-                <label for="studentindex">Search by Student Index</label><br>
-                <input type="text" id="studentIndex"><br>
-            
-            </form>
-            <br>
-            <a href="mmStudentDetailsV.php" ><button type="submit" name="" >Display Student's Details</button></a><br>
-            <!-- mekedi js function eka check krla tamai display kranna one -->
-            </div>
-        <div class="footer">
-        <?php
-              require_once('../include/footer.php');
+            <div class="side-nav">
+            <?php 
+              require('../mahapolaSchemeMaintainer/mmSideNavV.php');
             ?>
-        </div>
-    </div>
-</body>
-</html>
+            </div>
+            
+
+            <div class="content">
+                <div>
+                    <h4>Mark Mahapola Selected Students</h4>
+                </div>
+
+                <form action="" method="POST">
+
+                    <!-- <input type="radio" id="studentindex" name="student" value="studentindex"> -->
+                    <label for="studentindex">Search by Student Index</label><br>
+                    <input type="text" id="studentIndex"><br>
+                </form>
+
+                <br>
+                <a href="mmStudentDetailsV.php" ><button type="submit" name="" >Display Student's Details</button></a><br>
+                <!-- mekedi js function eka check krla tamai display kranna one -->
+            </div>
+
+            <div class="right-side-bar">
+                <a href="../../controller/viewProfileController.php?user_id=<?php echo $_SESSION['userId'] ?>"><button type="submit" name="" class="button">Profile</button></a>
+            </div>
+            
+            <?php
+                require_once('../basic/footer.php');
+            ?>
+      
+
+</main>
+
+
+
+
+
+
+
+
+
--- view/mahapolaSchemeMaintainer/mmUpdateProfileV.php
+++ view/mahapolaSchemeMaintainer/mmUpdateProfileV.php
@@ -1,77 +1,80 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Update Profile</title>
-    <link rel="stylesheet" href="../css/main.css">
-</head>
-<body>
-    <div class="container">
-        <div class="header">
-            <!-- <div class="nameLogo"> -->
-                <img src="../img/ims.jpg" alt="ims" class="logo">
-            <!-- </div> -->
-            <div class="options">
-                <a href="mmHomeV.php">Home</a>
-                <a href="mmProfileV.php">Profile</a>
-                <a href="#">Logout</a>
-            </div>
-        </div>
+<main>
+        <title>Update Profile</title>
+
+<?php
+    require('../basic/header.php');
+    
+?>
+
         
-        <!-- breadcrumbs -->
-        <ul class="breadcrumb">
-            <li><a href="mmHomeV.php">Home</a></li>
-            <li><a href="mmProfileV.php">Profile</a></li>
-            <li>Update Profile</li>
-        </ul>
-        
-        <?php
-          require_once('mmSideNavV.php');
-        ?>
-        
-        <div class="banner">
-            <div>
-                <h2>Mahapola Scheme Maintainer</h2>
+            <div class="header">Breadcrumbs
+                <!-- <ul class="breadcrumb">
+                    <li><a href="mmHomeV.php">Home</a></li>
+                    <li><a href="mmProfileV.php">Profile</a></li>
+                    <li>Update Profile</li>
+                </ul> -->
             </div>
-            
-        </div>
-        <div class="content">
-            <div>
-                <h3>Update Profile</h3>
+
+            <div class="side-nav">
+                <?php 
+                    require('../mahapolaSchemeMaintainer/mmSideNavV.php');
+                ?>
             </div>
-            <div>
-                <form action="" method="post">
-                    <label for="empId">Employee Id</label>
-                    <input type="text" value=""> <br>
-                    <label for="nameWithInt">Initials of the name</label>
-                    <input type="text" value=""> <br>
-                    <label for="surname">Surname</label>
-                    <input type="text" value=""> <br>
-                    <label for="email">Email</label>
-                    <input type="email" value=""> <br>
-                    <label for="mobNum">Mobile Number</label>
-                    <input type="text" value=""> <br>
-                    <label for="telNum">Telephone Number</label>
-                    <input type="text" value=""> <br>
-                    <label for="dob">Date of Birth</label>
-                    <input type="date" value=""> <br>
-                    <label for="designation">Designation</label>
-                    <input type="text" value=""> <br>
-                    <label for="appointmentDate">Appointment Date</label>
-                    <input type="date" value=""> <br>                    
-                </form>
-                <a href="mmUpdateProfileSuccessV.php"><button type="submit">Save Updates</button></a>
-            </div>
+
+            <div class="content">
+                <div>
+                    <h3>Update Profile</h3>
+                </div>
+                <div>
+                    <form action="" method="post">
+                        <label for="empId">Employee Id</label>
+                        <input type="text" value=""> <br>
+                        <label for="nameWithInt">Initials of the name</label>
+                        <input type="text" value=""> <br>
+                        <label for="surname">Surname</label>
+                        <input type="text" value=""> <br>
+                        <label for="email">Email</label>
+                        <input type="email" value=""> <br>
+                        <label for="mobNum">Mobile Number</label>
+                        <input type="text" value=""> <br>
+                        <label for="telNum">Telephone Number</label>
+                        <input type="text" value=""> <br>
+                        <label for="dob">Date of Birth</label>
+                        <input type="date" value=""> <br>
+                        <label for="designation">Designation</label>
+                        <input type="text" value=""> <br>
+                        <label for="appointmentDate">Appointment Date</label>
+                        <input type="date" value=""> <br>                    
+                    </form>
+                    <a href="mmUpdateProfileSuccessV.php"><button type="submit">Save Updates</button></a>
+                </div>
 
                     <!-- mekedi api database eken gnna data tika for each loop ekk ghla display krnna ona habai mekedi update krnna puluwn wenna ona -->
 
-        </div>
-        <div class="footer">
+            </div>
+            
+            
+            <div class="right-side-bar">
+                <a href="../../controller/viewProfileController.php?user_id=<?php echo $_SESSION['userId'] ?>"><button type="submit" name="" class="button">Profile</button></a>
+            </div>
+            
             <?php
-                require_once('../include/footer.php');
+                require_once('../basic/footer.php');
             ?>
-        </div>
-    </div>
-</body>
-</html>
+      
+
+</main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
