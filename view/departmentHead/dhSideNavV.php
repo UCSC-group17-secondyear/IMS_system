bfda96df983ad4bcb7b4d7163b2fd4b8db366f4f
malithya_fernando <malithyafernando@gmail.com>
2020-10-12 19:19:18 +0530

    Update Side Nav

--- view/departmentHead/dhSideNavV.php
+++ view/departmentHead/dhSideNavV.php
@@ -1,17 +1,9 @@
 <main>
-    <a href="dhMembRequestFormV.php">
-            <button type="submit" name="" class="button"> View Memebership Request Forms </button>
-    </a> <br>
+    <a href="dhMembRequestFormV.php"><button type="submit" name="" class="button"> View Memebership Request Forms </button></a> <br>
 
-    <a href="dhCertifiedFormV.php">
-            <button type="submit" name="" class="button"> View Certified Memebership Forms </button>
-    </a> <br>
+    <a href="dhCertifiedFormV.php"><button type="submit" name="" class="button"> View Certified Memebership Forms </button></a> <br>
 
-    <a href="dhMedicalSchemDetailsV.php">
-            <button type="submit" name="" class="button"> View Scheme Details </button>
-    </a> <br>
+    <a href="dhMedicalSchemDetailsV.php"><button type="submit" name="" class="button"> View Scheme Details </button></a> <br>
 
-    <a href="dhRegisterMedicalScheme.php">
-            <button type="submit" name="" class="button"> Register to the Staff Medical Scheme </button>
-    </a> <br>
+    <a href="../../controller/registerMSController.php?user_id=<?php echo $_SESSION['userId'] ?>"><button type="submit" name="" class="button">Register to the Staff Medical Scheme</button></a>
 </main>
