4127e9aab576bf249697470c33aa2b70028c5701
chathuRashmini <chathu.rash97@gmail.com>
2020-10-13 19:11:55 +0530

    Updated view claim details functionality

--- view/academicStaffMember/asmViewSchemeDetailsV.php
+++ view/academicStaffMember/asmViewSchemeDetailsV.php
@@ -16,28 +16,10 @@
             require '../academicStaffMember/asmSideNavV.php';
         ?>
     </div>
-
-    <div class="content">
-        <div>
-            <h3>Scheme Details</h3>
-        </div>
-        <h4>Scheme 1 : </h4>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, voluptas cupiditate tenetur incidunt
-                ab autem maiores optio inventore sunt dolor. Voluptate tenetur totam similique molestias labore ipsum
-                architecto eius amet.</p>
-
-        <h4>Scheme 2 : </h4>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, voluptas cupiditate tenetur incidunt
-                ab autem maiores optio inventore sunt dolor. Voluptate tenetur totam similique molestias labore ipsum
-                architecto eius amet.</p>
-
-        <h4>Scheme 3 : </h4>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, voluptas cupiditate tenetur incidunt
-                ab autem maiores optio inventore sunt dolor. Voluptate tenetur totam similique molestias labore ipsum
-                architecto eius amet.</p>
-
-        <a href="asmHomeV.php"><button type="submit" name="">OK</button></a>
-    </div>
+    
+    <?php
+        require '../basic/viewClaimDetails.php';
+    ?>
 
     <div class="right-side-bar">
         <a href="../../controller/viewProfileController.php?user_id=<?php echo $_SESSION['userId'] ?>"><button type="submit" name="" class="button">Profile</button></a>
