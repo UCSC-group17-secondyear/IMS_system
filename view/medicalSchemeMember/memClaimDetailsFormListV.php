c3be3d959a04a01692887331a8b37b564589a302
chathuRashmini <chathu.rash97@gmail.com>
2020-10-13 06:40:01 +0530

    Added breadcrumbs to medical scheme member

--- view/medicalSchemeMember/memClaimDetailsFormListV.php
+++ view/medicalSchemeMember/memClaimDetailsFormListV.php
@@ -1,55 +1,39 @@
 <main>
     <title>Claim Details</title>
 
-<?php
-    require('../basic/header.php');
-    
-?>
-
+    <?php
+        require('../basic/header.php');
         
-            <div class="header">Breadcrumbs
-                <!-- <ul class="breadcrumb">
-                    <li><a href="memHomeV.php">Home</a></li>
-                    <li><a href="memClaimFormListV.php">Form List</a></li>
-                    <li>Form</li>
-                </ul> -->
-            </div>
+    ?>
+        
+    <div class="header">
+        <ul class="breadcrumbs">
+            <li><a href="memHomeV.php">Home</a></li>
+            <li><a href="memClaimFormListV.php">Claim Forms</a></li>
+            <li>Selected Form</li>
+        </ul>
+    </div>
 
-            <div class="side-nav">
-                <?php 
-                    require('../medicalSchemeMember/memSideNavV.php');
-                ?>
-            </div>
+    <div class="side-nav">
+        <?php 
+            require('../medicalSchemeMember/memSideNavV.php');
+        ?>
+    </div>
 
-            <div class="content">
-                <div>
-                    <h4>Claim Details</h4>
-                </div>
+    <div class="content">
+        <div>
+            <h4>Claim Details</h4>
+        </div>
 
-                <!-- pdf ekak widiyat generate kranna one claim details -->
+        <a href="memClaimFormListV.php"><button type="submit" name="">OK</button></a><br>
 
-                <a href="memClaimFormListV.php"><button type="submit" name="">OK</button></a><br>
+    </div>
 
-            </div>
-
-            <div class="right-side-bar">
-                <a href="../../controller/viewProfileController.php?user_id=<?php echo $_SESSION['userId'] ?>"><button type="submit" name="" class="button">Profile</button></a>
-            </div>
-            
-            <?php
-                require_once('../basic/footer.php');
-            ?>
-      
-
+    <div class="right-side-bar">
+        <a href="../../controller/viewProfileController.php?user_id=<?php echo $_SESSION['userId'] ?>"><button type="submit" name="" class="button">Profile</button></a>
+    </div>
+    
+    <?php
+        require_once('../basic/footer.php');
+    ?>
 </main>
-
-
-
-
-
-
-
-
-
-
-
--- view/medicalSchemeMember/memFormUpdateSuccessV.php
+++ view/medicalSchemeMember/memFormUpdateSuccessV.php
@@ -1,44 +1,36 @@
 <main>
     <title>Update Successfull</title>
 
-<?php
-    require('../basic/header.php');
+    <?php
+        require('../basic/header.php');
+    ?>
+
+    <div class="header">
+        <ul class="breadcrumbs">
+            <li><a href="memHomeV.php">Home</a></li>
+            <li><a href="memUpdateClaimFormsV.php">Select Form</a></li>
+            <li>Form</li>
+        </ul>
+    </div>
+
+    <div class="side-nav">
+        <?php 
+            require('../medicalSchemeMember/memSideNavV.php');
+        ?>
+    </div>
     
-?>
+    <div class="content">
+        <p>Your Form has been Submitted Succesfully.</p>
+        <!-- form eka updat kalat passe mekay redirect wenne -->
 
-        
-            <div class="header">Breadcrumbs
-                <!-- <ul class="breadcrumb">
-                    <li><a href="memHomeV.php">Home</a></li>
-                    <li><a href="memUpdateClaimFormsV.php">Select Form</a></li>
-                    <li>Form</li>
-                </ul> -->
-            </div>
-
-            <div class="side-nav">
-                <?php 
-                    require('../medicalSchemeMember/memSideNavV.php');
-                ?>
-            </div>
-            
-            <div class="content">
-                <p>Your Form has been Submitted Succesfully.</p>
-                <!-- form eka updat kalat passe mekay redirect wenne -->
-
-                <a href="memUpdateClaimFormsV.php"><button type="submit" name="">OK</button></a><br>
-            </div>
-         
-            <div class="right-side-bar">
-                <a href="../../controller/viewProfileController.php?user_id=<?php echo $_SESSION['userId'] ?>"><button type="submit" name="" class="button">Profile</button></a>
-            </div>
-            
-            <?php
-                require_once('../basic/footer.php');
-            ?>
-      
-
+        <a href="memUpdateClaimFormsV.php"><button type="submit" name="">OK</button></a><br>
+    </div>
+ 
+    <div class="right-side-bar">
+        <a href="../../controller/viewProfileController.php?user_id=<?php echo $_SESSION['userId'] ?>"><button type="submit" name="" class="button">Profile</button></a>
+    </div>
+    
+    <?php
+        require_once('../basic/footer.php');
+    ?>
 </main>
-
-
-
-
