0153fc8bf25129ed7a0b609188004daf59c137bb
Janith Renuka <56946043+janithrenuka@users.noreply.github.com>
2020-10-14 09:27:38 +0530

    Change Side Nav & Model

--- model/Model.php
+++ model/Model.php
@@ -90,32 +90,11 @@
 			return $result;
 		}
 
-		public static function registerMS($user_id, $department, $health_condition, $civil_status, $scheme_name, $connect)
-		{
-			$query = "INSERT INTO tbl_user_flag (user_id, department, healthcondition, civilstatus, schemename) VALUES('$user_id', '$department', '$health_condition', '$civil_status', '$scheme_name')";
+		
 
-			$result = mysqli_query($connect, $query);
+		
 
-			return $result;
-		}
-
-		public static function department($connect)
-		{
-			$query = "SELECT department FROM tbl_department";
-
-			$result_set = mysqli_query($connect, $query);
-			
-			return $result_set;
-		}
-
-		public static function scheme($connect)
-		{
-			$query = "SELECT schemename FROM tbl_medicalscheme";
-			
-			$result_set = mysqli_query($connect, $query);
-			
-			return $result_set;
-		}
+		
 
 		public static function viewMember($user_id, $connect){ // for opd form & surgical form
 			$query = "SELECT * FROM users WHERE userId={$user_id} LIMIT 1";
