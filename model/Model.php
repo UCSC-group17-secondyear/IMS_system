b6783a0f390173c09c511fb2fbdd6c517ae6c457
milanmalshika <milanmalshika22@gmail.com>
2020-10-15 23:24:43 +0530

    Create Manage Semester Flow

--- model/Model.php
+++ model/Model.php
@@ -247,5 +247,50 @@
 					
 			return $result_set;
 		}
+
+		public static function enterSemester($semester, $academic_year, $start_date, $end_date, $connect)
+		{
+			$query = "INSERT INTO tbl_semester (semester, academic_year, start_date, end_date ) VALUES('$semester', '$academic_year', '$start_date', '$end_date')";
+
+			$result = mysqli_query($connect, $query);
+
+			return $result;
+		}
+
+		public static function viewSemesters($connect) 
+		{
+			$query = "SELECT * FROM tbl_semester WHERE is_deleted=0 ORDER BY sem_id";
+
+			$result_set = mysqli_query($connect, $query);
+
+			return $result_set;
+		}
+
+		public static function viewASem ($sem_id, $connect) 
+		{
+			$query = "SELECT * FROM tbl_semester WHERE sem_id={$sem_id} LIMIT 1";
+
+			$result_set = mysqli_query($connect, $query);
+
+			return $result_set;
+		}
+
+		public static function updateSemester($sem_id, $semester, $academic_year, $start_date, $end_date, $connect)
+		{
+			$query = "UPDATE tbl_semester SET semester='{$semester}', academic_year='{$academic_year}', start_date='{$start_date}', end_date='{$end_date}' WHERE sem_id={$sem_id} LIMIT 1";
+
+			$result = mysqli_query($connect, $query);
+
+			return $result;
+		}
+
+		public static function deleteSemester($sem_id, $connect)
+		{
+			$query = "UPDATE tbl_semester SET is_deleted = 1 WHERE sem_id={$sem_id} LIMIT 1";
+
+			$result = mysqli_query($connect, $query);
+
+			return $result;
+		}
 	}
 ?>
