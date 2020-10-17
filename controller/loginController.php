9fc7247aac0b84c89e759fdbf3891d44f82fc91e
milanmalshika <milanmalshika22@gmail.com>
2020-10-17 18:04:56 +0530

    Update loginController.php

--- controller/loginController.php
+++ controller/loginController.php
@@ -61,11 +61,11 @@
 						header('Location:../view/medicalOfficer/moHomeV.php');
 					}
 					else {
-						echo "USER";
+						echo "Admin have not yet selected the user role to you.";
 					}
                 }
                 else if(mysqli_num_rows($result)==0){
-                    echo "No row bla bla bla";
+                    echo "No result.";
                 }
             }
             else {
