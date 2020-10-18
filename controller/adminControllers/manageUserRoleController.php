0edc2ea0a6946d34caf239b98dec4f5537ebf864
chathuRashmini <chathu.rash97@gmail.com>
2020-10-18 08:13:43 +0530

    Updated 'manage user roles' functionality

--- controller/adminControllers/manageUserRoleController.php
+++ controller/adminControllers/manageUserRoleController.php
@@ -1,4 +1,5 @@
 <?php 
+    session_start();
     require_once('../../model/adminModel/manageUserRolesModel.php');
     require_once('../../config/database.php');
 
@@ -22,6 +23,7 @@
                 $_SESSION['user_role'] .= "<tr>";
                 $_SESSION['user_role'] .= "<td>{$roles['role_name']}</td>";
                 $_SESSION['user_role'] .= "<td>{$roles['description']}</td>";
+                $_SESSION['user_role'] .= "</tr>";
 
                 header('Location:../../view/admin/aViewUserRolesV.php');
             }
