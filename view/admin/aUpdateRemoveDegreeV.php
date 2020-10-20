bffdaf85931f57dbd552611cb7d7bf6837d2cd21
milanmalshika <milanmalshika22@gmail.com>
2020-10-20 14:40:30 +0530

    Updated

--- view/admin/aUpdateRemoveDegreeV.php
+++ view/admin/aUpdateRemoveDegreeV.php
@@ -18,17 +18,17 @@
     </div>
 
     <div class="content">
-        <div>
-            <h3>Update or remove a degree</h3>
-        </div>
-        
-        Enter degree name <input type="text" name="degree" placeholder="Degree name" required/> <br>
-        
-        Enter Degree description <input type="text" name="description" placeholder="Degree description" required/> <br>
-        
-        <button type="submit" name="updateDegree-submit">Update degree</button>
-        
-        <button type="submit" name="removeDegree-submit">Remove degree</button>
+       <h1>Department Details</h1>
+
+        <table class="mytable">
+            <tr>
+                <th>Degree</th>
+                <th>Degree Abbriviation</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+            <?php echo $_SESSION['degree_list']; ?>
+        </table>
     </div>
 
     <?php
