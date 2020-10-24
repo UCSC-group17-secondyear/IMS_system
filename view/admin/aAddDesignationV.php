9ae141e2f78d0e1f006fbb450163bfed38081b88
chathuRashmini <chathu.rash97@gmail.com>
2020-10-24 16:17:43 +0530

    Updated css to admin's ui

--- view/admin/aAddDesignationV.php
+++ view/admin/aAddDesignationV.php
@@ -1,37 +1,55 @@
+<?php
+    require '../basic/topnav.php';
+?>
+
 <main>
     <title>Add a designation</title>
-    <?php
-        require '../basic/header.php';
-    ?>
 
-    <div class="header">
+    <div class="sansserif">
         <ul class="breadcrumbs">
             <li><a href="aHomeV.php">Home</a></li>
             <li>Add a new Designation</li>
         </ul>
-    </div>
 
-    <div class="side-nav">
-        <?php
-            require '../admin/aSideNavV.php';
-        ?>
-    </div>
+        <div class="row">
+            <div class="col left20">
+                <?php
+                    require 'aSideNavV.php';
+                ?>
+            </div>
 
-    <div class="content">
-        <div>
-            <h3>Add a new Designation</h3>
+            <div class="col right80">
+                <div>
+                    <h2>Add a new Designation</h2>
+                </div>
+
+                <div class="contentForm">
+                    <form action="../../controller/aAddDesignationController.php" method="POST">
+                        <div class="row">
+                            <div class="col-25">
+                              <label>Designation Name</label>
+                            </div>
+                            <div class="col-75">
+                                <input type="text" name="designation" placeholder="Enter designation name" required/>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-25">
+                              <label>Description</label>
+                            </div>
+                            <div class="col-75">
+                                <input type="text" name="description" placeholder="Enter description" required/>
+                            </div>
+                        </div>
+                        <button class="mainbtn" type="submit" name="addDesignation-submit">Add Designation</button>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <form action="../../controller/aAddDesignationController.php" method="POST">
-            <label for="">Designation Name</label>
-            <input type="text" name="designation" placeholder="Enter designation name" required/><br>
-            <label for="">Description</label>
-            <input type="text" name="description" placeholder="Enter description" required/><br>                    
-            <button type="submit" name="addDesignation-submit">Add Designation</button>
-        </form>
     </div>
-
-    <?php
-        require '../basic/footer.php';
-    ?>
 </main>
+
+<?php
+    require '../basic/footer.php';
+?>
--- view/admin/aUpdateRemoveDepartmentV.php
+++ view/admin/aUpdateRemoveDepartmentV.php
@@ -1,38 +1,43 @@
+<?php
+    require '../basic/topnav.php';
+?>
+
 <main>
     <title>Update or Remove a department</title>
-    <?php
-        require '../basic/header.php';
-    ?>
 
-    <div class="header">
+    <div class="sansserif">
         <ul class="breadcrumbs">
             <li><a href="aHomeV.php">Home</a></li>
             <li>Update or remove a Department</li>
         </ul>
+
+        <div class="row">
+            <div class="col left20">
+                <?php
+                    require 'aSideNavV.php';
+                ?>
+            </div>
+
+            <div class="col right80">
+                <div>
+                    <h2>Department Details</h2>
+                </div>
+
+                <table id="tableStyle" class="mytable">
+                    <tr>
+                        <th>Department</th>
+                        <th>Department Head</th>
+                        <th>Description</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                    <?php echo $_SESSION['dept_list']; ?>
+                </table>
+            </div>
+        </div>
     </div>
-
-    <div class="side-nav">
-        <?php
-            require '../admin/aSideNavV.php';
-        ?>
-    </div>
-
-    <div class="content">
-        <h1>Department Details</h1>
-
-        <table class="mytable">
-            <tr>
-                <th>Department</th>
-                <th>Department Head</th>
-                <th>Description</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-            <?php echo $_SESSION['dept_list']; ?>
-        </table>
-    </div>
-
-    <?php
-        require '../basic/footer.php';
-    ?>
 </main>
+    
+<?php
+    require '../basic/footer.php';
+?>
