3c40ba63f26228539dff3aeb346431eb0f3822b3
malithya_fernando <malithyafernando@gmail.com>
2020-10-26 22:03:37 +0530

    Updated css in some view files under 'report viewer'

--- view/reportViewer/rvSideNavV.php
+++ view/reportViewer/rvSideNavV.php
@@ -1,39 +1,55 @@
 <main>
-    <button class="button accordion">View Student Attendance Reports</button>
-    <div class="panel">
-        <a href="rvStudentWiseAttendanceV.php" class="buttonTwo">View Attendance Student Wise</a>
-        <a href="rvMonthWiseAttendanceV.php" class="buttonTwo">View Attendance Montth Wise</a>
-        <a href="rvSubjectWiseAttendanceV.php" class="buttonTwo">View Attendance Subject Wise</a>
-        <a href="rvBatchWiseAttendanceV.php" class="buttonTwo">View Attendance Batch Wise</a>
-        <a href="rvSemesterWiseAttendanceV.php" class="buttonTwo">View Attendance Semester Wise</a>
+    <div class="sansserif">
+        <ul id="tree_view">
+            <li><button class="tree_list">>View Student Attendance Reports</button>
+                <ul class="tree_nest">
+                    <button>
+                        <a href="rvStudentWiseAttendanceV.php"><li><i class="fa fa-plus-circle"></i>View Attendance Student Wise</li></a>
+                    </button>
+                    <button>
+                        <a href="rvMonthWiseAttendanceV.php"><li><i class="fa fa-minus-circle"></i>View Attendance Montth Wise</li></a></button>
+                    </button>
+                    <button>
+                        <a href="rvSubjectWiseAttendanceV.php"><li><i class="fa fa-pencil-square-o"></i>View Attendance Subject Wise</li></a>
+                    </button>
+                    <button>
+                        <a href="rvBatchWiseAttendanceV.php"><li><i class="fa fa-pencil-square"></i>View Attendance Batch Wise</li></a>
+                    </button>
+                    <button>
+                        <a href="rvSemesterWiseAttendanceV.php"><li><i class="fa fa-pencil-square"></i>View Attendance Semester Wise</li></a>
+                    </button>
+                </ul>
+            </li>
+            <li>
+                <button type="submit" class="tree_list"><a href="">View Mahapola Nominated Student List</a></button> <br>
+            </li>
+            <li>
+                <button type="submit" class="tree_list"><a href="">View Reports in Mahapola Scheme</a></button> <br>
+            </li>
+            <li>
+                <button type="submit" class="tree_list"><a href="">View Member list of Medical Scheme</a></button> <br>
+            </li>
+            <li>
+                <button type="submit" class="tree_list"><a href="">View Forms of the Medical Scheme</a></button> <br>
+            </li>
+            <li>
+                <button type="submit" class="tree_list"><a href="">View Claim Details</a></button> <br>
+            </li>
+            <li>
+                <button type="submit" class="tree_list"><a href="../../controller/memControllers/registerMSController.php?user_id=<?php echo $_SESSION['userId'] ?>">Register to the Staff Medical Scheme</a></button> <br>
+            </li>
+        </ul>
     </div>
 
-    <a href=""><button type="submit" name="" class="button">View Mahapola Nominated Student List</button></a><br>
-
-    <a href=""><button type="submit" name="" class="button">View Reports in Mahapola Scheme</button></a><br>
-
-    <a href=""><button type="submit" name="" class="button">View Member list of Medical Scheme</button></a><br>
-
-    <a href=""><button type="submit" name="" class="button">View Forms of the Medical Scheme</button></a><br>
-
-    <a href=""><button type="submit" name="" class="button">View Claim Details</button></a><br>
-
-    <a href="../../controller/memControllers/registerMSController.php?user_id=<?php echo $_SESSION['userId'] ?>"><button type="submit" name="" class="button">Register to the Staff Medical Scheme</button></a>
-
     <script>
-    var acc = document.getElementsByClassName("accordion");
-    var i;
+        var toggler = document.getElementsByClassName("tree_list");
+        var i;
 
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-            } else {
-                panel.style.display = "block";
-            }
-        });
-    }
+        for (i = 0; i < toggler.length; i++) {
+          toggler[i].addEventListener("click", function() {
+            this.parentElement.querySelector(".tree_nest").classList.toggle("active");
+            this.classList.toggle("tree_list-down");
+          });
+        }
     </script>
 </main>
