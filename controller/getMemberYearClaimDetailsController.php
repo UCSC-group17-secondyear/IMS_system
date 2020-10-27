d533aaae7580f48d82b20efd3c4980a502fba45e
Janith Renuka <56946043+janithrenuka@users.noreply.github.com>
2020-10-27 17:30:41 +0530

    Update css & Year claim details

---
+++ controller/getMemberYearClaimDetailsController.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    require_once('../model/Model.php');
+    require_once('../config/database.php');
+?>
+
+<?php
+        if(isset($_GET['year-claim-submit'])){
+
+
+            
+        }
+?>
