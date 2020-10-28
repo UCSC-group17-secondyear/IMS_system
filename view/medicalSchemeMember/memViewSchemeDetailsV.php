4660277925dd750ce489bf7879ab1762b8d788b1
Janith Renuka <56946043+janithrenuka@users.noreply.github.com>
2020-10-28 05:59:35 +0530

    Update css in medical scheme member

--- view/medicalSchemeMember/memViewSchemeDetailsV.php
+++ view/medicalSchemeMember/memViewSchemeDetailsV.php
@@ -1,28 +1,32 @@
+<?php
+    require "../basic/topnav.php";
+?>
+
 <main>
     <title>Scheme Details</title>
+    <div class="sansserif">   
+            
+                <ul class="breadcrumbs">
+                    <li><a href="memHomeV.php">Home</a></li>
+                    <li>View Scheme Details</li>
+                </ul>
+            
+        <div class="row">
+            <div class="col left20">
+                <?php 
+                    require('memSideNavV.php');
+                ?>
+            </div>
 
-    <?php
-        require('../basic/header.php');
-    ?>
-        
-    <div class="header">
-        <ul class="breadcrumbs">
-            <li><a href="memHomeV.php">Home</a></li>
-            <li>View Scheme Details</li>
-        </ul>
+            <div class="col right80">
+                    <?php
+                        require '../basic/viewClaimDetails.php';
+                    ?>
+            </div>
+        </div>
     </div>
-
-    <div class="side-nav">
-        <?php 
-            require('../medicalSchemeMember/memSideNavV.php');
-        ?>
-    </div>
-
-    <?php
-        require '../basic/viewClaimDetails.php';
-    ?>
-    
-    <?php
-        require_once('../basic/footer.php');
-    ?>
 </main>
+
+<?php
+    require_once('../basic/footer.php');
+?>
--- view/medicalSchemeMember/memYearClaimDetailsV.php
+++ view/medicalSchemeMember/memYearClaimDetailsV.php
@@ -1,33 +1,35 @@
+<?php
+    require "../basic/topnav.php";
+?>
+
 <main>
     <title>Claim Details</title>
+        <div class="sansserif"> 
+                
+                    <ul class="breadcrumbs">
+                        <li><a href="memHomeV.php">Home</a></li>
+                        <li><a href="memGetClaimDetailsV.php">Enter Year</a></li>
+                        <li>Claim Details</li>
+                    </ul>
+                
+            <div class="row">
+                <div class="col left20">
+                    <?php 
+                        require('memSideNavV.php');
+                    ?>
+                </div>
 
-    <?php
-        require('../basic/header.php');
-    ?>
-        
-    <div class="header">
-        <ul class="breadcrumbs">
-            <li><a href="memHomeV.php">Home</a></li>
-            <li><a href="memGetClaimDetailsV.php">Enter Year</a></li>
-            <li>Claim Details</li>
-        </ul>
-    </div>
-
-    <div class="side-nav">
-        <?php 
-            require('../medicalSchemeMember/memSideNavV.php');
-        ?>
-    </div>
-
-    <div class="content">
-        <div>
-            <h4>Claim Details</h4>
+                <div class="col right80">
+                    <div>
+                        <h2>Claim Details</h2>
+                    </div>
+                    
+                    <a href="memGetClaimDetailsV.php"><button class="mainbtn" type="submit" name="">OK</button></a><br>
+                </div>
+            </div>
         </div>
-        <!-- memberge year eke claim details pdf ekak wiidyat show kranna one -->
-        <a href="memGetClaimDetailsV.php"><button type="submit" name="">OK</button></a><br>
-    </div>
-    
-    <?php
-        require_once('../basic/footer.php');
-    ?>
 </main>
+
+<?php
+    require_once('../basic/footer.php');
+?>
