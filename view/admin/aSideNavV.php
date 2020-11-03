cb271d010a7bbdedb1a6a5229b5992930fb33e3b
chathuRashmini <chathu.rash97@gmail.com>
2020-11-03 13:12:37 +0530

    Updated manage medical schemes functionality

--- view/admin/aSideNavV.php
+++ view/admin/aSideNavV.php
@@ -12,7 +12,7 @@
                         <a href="aAddNewUserRoleV.php"><li><i class="fa fa-plus-circle"></i>Add a new user role</li></a>
                     </button>
                     <button>
-                        <a href="aRemoveUserRoleV.php"><li><i class="fa fa-minus-circle"></i>Remove a user role</li></a>
+                        <a href="aRemoveUserRoleV.php"><li><i class="fa fa-trash"></i>Remove a user role</li></a>
                     </button>
                     <button>
                         <a href="../../controller/aAssignUserRoleControler.php"><li><i class="fa fa-pencil-square-o"></i>Assign a user role to a user</li></a>
@@ -32,16 +32,16 @@
             <li><button class="tree_list">Manage Medical Schemes</button>
                 <ul class="tree_nest">
                     <button>
-                        <a href="aViewSchemeDetailsV.php"><li><i class="fa fa-plus-circle"></i>View schemes</li></a>
+                        <a href="aViewSchemeDetailsV.php"><li><i class="fa fa-medkit"></i>View schemes</li></a>
                     </button>
                     <button>
                         <a href="aAddNewSchemeV.php"><li><i class="fa fa-plus-circle"></i>Add a new scheme</li></a>
                     </button>
                     <button>
-                        <a href="aUpdateSchemeV.php"><li><i class="fa fa-minus-circle"></i>Update Policies of a scheme</li></a
+                        <a href="aUpdateSchemeV.php"><li><i class="fa fa-pencil-square"></i>Update Policies of a scheme</li></a
                             ></button>
                     <button>
-                        <a href="aRemoveSchemeV.php"><li><i class="fa fa-pencil-square-o"></i>Remove a scheme</li></a>
+                        <a href="aRemoveSchemeV.php"><li><i class="fa fa-trash"></i>Remove a scheme</li></a>
                     </button>
                 </ul>
             </li>
@@ -55,7 +55,7 @@
                         <a href="aAddDegreeV.php"><li><i class="fa fa-plus-circle"></i>Add a new degree</li></a>
                     </button>
                     <button>
-                        <a href="../../controller/aViewDegreeController.php"><li><i class="fa fa-minus-circle"></i>Update or remove a degree</li></a>
+                        <a href="../../controller/aViewDegreeController.php"><li><i class="fa fa-pencil-square"></i>Update or remove a degree</li></a>
                     </button>
                 </ul>
             </li>
@@ -69,7 +69,7 @@
                         <a href="aAddSessionV.php"><li><i class="fa fa-plus-circle"></i>Add a new Session</li></a>
                     </button>
                     <button>
-                        <a href="aUpdateRemoveSessionV.php"><li><i class="fa fa-minus-circle"></i>Update or remove a Session type</li></a>
+                        <a href="aUpdateRemoveSessionV.php"><li><i class="fa fa-pencil-square"></i>Update or remove a Session type</li></a>
                     </button>
                 </ul>
             </li>
@@ -80,33 +80,33 @@
                         <a href="aAddSessionPerMonthV.php"><li><i class="fa fa-plus-circle"></i>Assign a monthly session</li></a>
                     </button>
                     <button>
-                        <a href="aUpdateRemoveSessionPerMonthV.php"><li><i class="fa fa-minus-circle"></i>Update or remove a Sessions Per Month</li></a>
+                        <a href="aUpdateRemoveSessionPerMonthV.php"><li><i class="fa fa-pencil-square"></i>Update or remove a Sessions Per Month</li></a>
                     </button>
                 </ul>
             </li>
             <li><button class="tree_list">Manage Semesters</button>
                 <ul class="tree_nest">
                     <button>
-                        <a href="../../controller/aViewSemesterController.php"><li><i class="fa fa fa-check-circle"></i>View Semesters</li></a>
+                        <a href="../../controller/aViewSemesterController.php"><li><i class="fa fa-graduation-cap"></i>View Semesters</li></a>
                     </button>
                     <button>
                         <a href="aAddSemesterV.php"><li><i class="fa fa-plus-circle"></i>Add a new Semester</li></a>
                     </button>
                     <button>
-                        <a href="../../controller/aUpSemesterController.php"><li><i class="fa fa-minus-circle"></i>Update or remove a Semester</li></a>
+                        <a href="../../controller/aUpSemesterController.php"><li><i class="fa fa-pencil-square"></i>Update or remove a Semester</li></a>
                     </button>
                 </ul>
             </li>
             <li><button class="tree_list">Manage Halls</button>
                 <ul class="tree_nest">
                     <button>
-                        <a href="../../controller/aViewHallController.php"><li><i class="fa fa fa-check-circle"></i>View Halls</li></a>
+                        <a href="../../controller/aViewHallController.php"><li><i class="fa fa-building"></i>View Halls</li></a>
                     </button>
                     <button>
                         <a href="aAddHallV.php"><li><i class="fa fa-plus-circle"></i>Add a new Hall</li></a>
                     </button>
                     <button>
-                        <a href="../../controller/aUpHallController.php"><li><i class="fa fa-minus-circle"></i>Update or remove a Hall</li></a>
+                        <a href="../../controller/aUpHallController.php"><li><i class="fa fa-pencil-square"></i>Update or remove a Hall</li></a>
                     </button>
                 </ul>
             </li>
@@ -119,20 +119,20 @@
                         <a href="aAddDepartmentV.php"><li><i class="fa fa-plus-circle"></i>Add a new Department</li></a>
                     </button>
                     <button>
-                        <a href="../../controller/aUpDepartmentController.php"><li><i class="fa fa-minus-circle"></i>Update or remove a Department</li></a>
+                        <a href="../../controller/aUpDepartmentController.php"><li><i class="fa fa-pencil-square"></i>Update or remove a Department</li></a>
                     </button>
                 </ul>
             </li>
             <li><button class="tree_list">Manage Designations</button>
                 <ul class="tree_nest">
                     <button>
-                        <a href="../../controller/aViewDesignationController.php"><li><i class="fa fa fa-check-circle"></i>View Designation</li></a>
+                        <a href="../../controller/aViewDesignationController.php"><li><i class="fa fa-user"></i>View Designation</li></a>
                     </button>
                     <button>
                         <a href="aAddDesignationV.php"><li><i class="fa fa-plus-circle"></i>Add a new Designation</li></a>
                     </button>
                     <button>
-                        <a href="../../controller/aUpDesignationController.php"><li><i class="fa fa-minus-circle"></i>Update or remove a Designation</li></a>
+                        <a href="../../controller/aUpDesignationController.php"><li><i class="fa fa-pencil-square"></i>Update or remove a Designation</li></a>
                     </button>
                 </ul>
             </li>
