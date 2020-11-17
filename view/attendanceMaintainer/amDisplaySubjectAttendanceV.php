20809af9c13886cf9b5d0e4190b0be1101238589
chathuRashmini <chathu.rash97@gmail.com>
2020-11-17 15:51:02 +0530

    Finished connecting attendance maintainer's ui flow

--- view/attendanceMaintainer/amDisplaySubjectAttendanceV.php
+++ view/attendanceMaintainer/amDisplaySubjectAttendanceV.php
@@ -9,7 +9,7 @@
             <li><a href="amHomeV.php">Home</a></li>
         <li><a href="amStudentWiseAttendanceV.php">Studentwise Attendance</a></li>
         <!-- <li><a href="amGetStdStdwiseAttendanceV.php">Filter student details</a></li> -->
-        <li class="active">Studentwise Attendance</li>
+        <li class="active">Subject-wise Attendance</li>
         </ul>
 
         <div class="row">
@@ -21,20 +21,22 @@
 
             <div class="col right80">
                 <div>
-                    <h2>Studentwise Attendance</h2>
+                    <h2>Subject-wise Attendance</h2>
                 </div>
 
                 <table id="tableStyle" class="mytable">
                     <tr>
                         <th>Student Index</th>
-                        <th>Subject Code</th>
                         <th>Attendance</th>
                     </tr>
                 </table>
 
-                <button class="mainbtn">
-                    <a href="amStudentWiseAttendanceV.php">View another studentwise attendance</a>
+                <button class="subbtn">
+                    <a href="amSubjectWiseAttendanceV.php">View another subject-wise attendance</a>
                 </button>
+                <button class="cancelbtn" type="submit" name="cancel-submit">
+                        <a href="amHomeV.php">Cancel</a> 
+                    </button>
             </div>
         </div>
     </div>
