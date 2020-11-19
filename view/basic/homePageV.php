89e44db31478d8581116efeafb1cd123896a730b
milanmalshika <milanmalshika22@gmail.com>
2020-11-19 13:32:57 +0530

    Update signup controller

--- view/basic/homePageV.php
+++ view/basic/homePageV.php
@@ -1,8 +1,13 @@
 <?php
-	require "topnav.php";
+	// require "topnav.php";
 ?>
 
 <main>	
+	<link rel="icon" href="../assests/img/favicon1.png">
+    <link rel="stylesheet" type="text/css" href="../assests/css/new.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 	<div class="home_image">
 		<div class="heading">
 			<h2>Information Management System</h2>
