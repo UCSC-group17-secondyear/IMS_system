222507a15eeb6aec4115e21fd5191311c7b123d9
chathuRashmini <chathu.rash97@gmail.com>
2020-11-22 14:37:38 +0530

    Updated am's ui flow-5

--- view/attendanceMaintainer/amProfileUpdatedV.php
+++ view/attendanceMaintainer/amProfileUpdatedV.php
@@ -12,7 +12,7 @@
             <li class="active">Action was success!</li>
         </ul>
     
-        <div class="row">
+        <div class="row" style="margin-bottom: 4%;" >
             <div class="col left20">
                 <?php
                     require 'amSideNavV.php';
