901a9542e456f69f6bf98a32fe54d3644e62163a
malithya_fernando <56259098+MalithyaFernando@users.noreply.github.com>
2021-03-20 12:24:20 +0530

    Update manage time table function related files - 3

--- controller/hamControllers/hamManageWeeklyTTC.php
+++ controller/hamControllers/hamManageWeeklyTTC.php
@@ -4,46 +4,53 @@
     require_once('../../model/hamModel/hamManageWeeklyTTModel.php');
 
     if(isset($_POST['entertt-submit'])) {
-        $records3 = hamModel::getAllsemesters($connect);
-        $records4 = hamModel::getAlldegrees($connect);
-        $_SESSION['semesters'] = "";
-        $_SESSION['degree'] = "";
         
-        if ($records3 && $records4) {
+        $semesters = hamModel::getAllsemesters($connect);
+        $degrees = hamModel::getAlldegrees($connect);
+        $_SESSION['acayear'] = "";
+        $_SESSION['semester'] = "";
+        $_SESSION['acayear_with_sem'] = "";
 
-            while ($record3 = mysqli_fetch_array($records3)) {
-                $_SESSION['semesters'] .= "<option value='".$record3['semester']."'>".$record3['semester']."</option>";
-            }
-
-            while ($record4 = mysqli_fetch_array($records4)) {
-                $_SESSION['degree'] .= "<option value='".$record4['degree_name']."'>".$record4['degree_name']."</option>";
-            }
-
-            header('Location:../../view/hallAllocationMaintainer/hamEnterTimeTableSelectV.php');
-        }
-    } elseif (isset($_POST['entertt-submit'])) {
-        $records1 = hamModel::getAllsubjects($connect);
-        $records2 = hamModel::getAllHalls($connect);
+        $subjects = hamModel::getAllsubjects($connect);
+        $halls = hamModel::getAllHalls($connect);
+        
         $_SESSION['subjects'] = "";
         $_SESSION['subject_code'] = "";
         $_SESSION['subject_with_code'] = "";
+        
         $_SESSION['allhalls'] = "";
+        $_SESSION['degree'] = "";
+        $_SESSION['wtt'] = "";
+        
+        if ($semesters && $degrees && $subjects && $halls) {
+            while ($sem = mysqli_fetch_array($semesters)) {
+                $_SESSION['acayear'] .= $sem['academic_year'];
+                $_SESSION['semester'] .= $sem['semester'];
+                $_SESSION['acayear_with_sem'] .= "<option value='".$sem['semester']."'>".$sem['academic_year']." - ".$sem['semester']."</option>";
+            }
 
-        if($records1 && $records2){
-            while ($record1 = mysqli_fetch_array($records1)) {
-                $_SESSION['subjects'] .= $record1['subject_name'];
-                $_SESSION['subject_code'] .= $record1['subject_code'];
-                $_SESSION['subject_with_code'] .= "<option value='".$record1['subject_code']."'>".$record1['subject_code']." - ".$record1['subject_name']."</option>";
+            while ($deg = mysqli_fetch_array($degrees)) {
+                $_SESSION['degree'] .= "<option value='".$deg['degree_name']."'>".$deg['degree_name']."</option>";
+            }
+
+            while ($sub = mysqli_fetch_array($subjects)) {
+                $_SESSION['subjects'] .= $sub['subject_name'];
+                $_SESSION['subject_code'] .= $sub['subject_code'];
+                $_SESSION['subject_with_code'] .= "<option value='".$sub['subject_code']."'>".$sub['subject_code']." - ".$sub['subject_name']."</option>";
             }
     
-            while ($record2 = mysqli_fetch_array($records2)) {
-                $_SESSION['allhalls'] .= "<option value='".$record2['hall_name']."'>".$record2['hall_name']."</option>";
+            while ($hall = mysqli_fetch_array($halls)) {
+                $_SESSION['allhalls'] .= "<option value='".$hall['hall_name']."'>".$hall['hall_name']."</option>";
             }
 
             header('Location:../../view/hallAllocationMaintainer/hamEnterTimeTableV.php');
         }
+    } elseif (isset($_POST['entertt-submit'])) {
 
-
+        $semester = mysqli_real_escape_string($connect, $_POST['semester']);
+        $degree = mysqli_real_escape_string($connect, $_POST['degree']);
+        $batch = mysqli_real_escape_string($connect, $_POST['batch']);
+    
     } elseif (isset($_POST['updateremovett-submit'])) {
 
     } 
--- view/hallAllocationMaintainer/hamEnterTimeTableV.php
+++ view/hallAllocationMaintainer/hamEnterTimeTableV.php
@@ -22,80 +22,118 @@
                     <h2>Enter Time Table</h2>
                 </div>
 
-                <div class="contentForm">
+                <div class="contentForm differentForm">
                     <form action="../../controller/hamControllers/hamManageWeeklyTTC.php" method="POST">
-                        <div class="row">
+                    <div class="row">
                             <div class="col-25">
-                                <label>Subject</label>
+                                <label>Semester</label>
                             </div>
                             <div class="col-75">
-                                <select name="subject" id="" required>
-                                    <option value="" >Select a Subject</option>
-                                    <?php echo $_SESSION['subject_with_code'] ?>
+                                <select name="semester" id="" required>
+                                    <option value="">Select the Semester</option>
+                                    <?php echo $_SESSION['acayear_with_sem'] ?>
                                 </select>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-25">
-                                <label>Hall</label>
+                                <label>Degree</label>
                             </div>
                             <div class="col-75">
-                                <select name="hall" id="" required>
-                                    <option value="">Select a Hall</option>
-                                    <?php echo $_SESSION['allhalls']?>
+                                <select name="degree" id="" required>
+                                    <option value="">Select the degree</option>
+                                    <?php echo $_SESSION['degree'] ?>
                                 </select>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-25">
-                                <label>Day</label>
+                                <label>Batch</label>
                             </div>
                             <div class="col-75">
-                                <select name="day" id="" required>
-                                    <option value="">Select a Day</option>
-                                    <option value="Monday">Monday</option>
-                                    <option value="Tuesday">Tuesday</option>
-                                    <option value="Wednesday">Wednesday</option>
-                                    <option value="Thursday">Thursday</option>
-                                    <option value="Friday">Friday</option>
+                                <select name="batch" id="" required>
+                                    <option value="">Select the Batch</option>
+                                    <option value="y1">1st year</option>
+                                    <option value="y2">2nd year</option>
+                                    <option value="y3">3rd year</option>
+                                    <option value="y4">4th year</option>
                                 </select>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-25">
-                                <label>Start time</label>
-                            </div>
-                            <div class="col-75">
-                                <input type="time" id="" name="start_time" required /><br>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-25">
-                                <label>End time</label>
-                            </div>
-                            <div class="col-75">
-                                <input type="time" id="" name="end_time" required /><br>
-                            </div>
-                        </div>
-                        <button class="subbtn" type="submit" name="entertt-submit">
-                            <a href="#">Enter</a>
-                        </button>
-                        <button class="cancelbtn" type="submit">
-                            <a href="hamHomeV.php">Cancel</a>
-                        </button>
+                        <table id="tableStyle">
+                            <tr>
+                                <th name="start_time">Start time</th>
+                                <th name="end_time">End time</th>
+                                <th name="Monday">Monday</th>
+                                <th name="Tuesday">Tuesday</th>
+                                <th name="Wednesday">Wedensday</th>
+                                <th name="Thursday">Thursday</th>
+                                <th name="Friday">Friday</th>
+                                <th></th>
+                            </tr>
+                            <tr>
+                                <td><input type="time" name="tt[i][]" value="i-start-time"/></td>
+                                <td><input type="time" name="tt[i][]" value="i-end-time"/></td>
+                                <td>
+                                    <select name="tt[i][]" id="mon" required>
+                                        <option value="" >Select Subject</option>
+                                        <?php echo $_SESSION['subject_with_code'] ?>
+                                    </select>
+                                    <select name="hall" id="mon" required>
+                                        <option value="">Select Hall</option>
+                                        <?php echo $_SESSION['allhalls']?>
+                                    </select>
+                                </td>
+                                <td>
+                                    <select name="tt[i][]" id="tues" required>
+                                        <option value="" >Select Subject</option>
+                                        <?php echo $_SESSION['subject_with_code'] ?>
+                                    </select>
+                                    <select name="hall" id="tues" required>
+                                        <option value="">Select Hall</option>
+                                        <?php echo $_SESSION['allhalls']?>
+                                    </select>
+                                </td>
+                                <td>
+                                    <select name="tt[i][]" id="wed" required>
+                                        <option value="" >Select Subject</option>
+                                        <?php echo $_SESSION['subject_with_code'] ?>
+                                    </select>
+                                    <select name="hall" id="wed" required>
+                                        <option value="">Select Hall</option>
+                                        <?php echo $_SESSION['allhalls']?>
+                                    </select>
+                                </td>
+                                <td>
+                                    <select name="tt[i][]" id="thurs" required>
+                                        <option value="" >Select Subject</option>
+                                        <?php echo $_SESSION['subject_with_code'] ?>
+                                    </select>
+                                    <select name="hall" id="thurs" required>
+                                        <option value="">Select Hall</option>
+                                        <?php echo $_SESSION['allhalls']?>
+                                    </select>
+                                </td>
+                                <td>
+                                    <select name="tt[i][]" id="fri" required>
+                                        <option value="" >Select Subject</option>
+                                        <?php echo $_SESSION['subject_with_code'] ?>
+                                    </select>
+                                    <select name="hall" id="fri" required>
+                                        <option value="">Select Hall</option>
+                                        <?php echo $_SESSION['allhalls']?>
+                                    </select>
+                                </td>
+                                <td>
+                                    <button class=".green" type="submit" name="addtt-submit"><a href="#">Add</a></button>
+                                </td>
+                            </tr>
+                            <?php //echo $_SESSION['wtt']; ?>
+                        </table>
+                        <button class="subbtn" type="submit" name="entertt-submit"><a href="#">Enter</a></button>
+                        <button class="cancelbtn" type="submit"><a href="hamHomeV.php">Cancel</a></button>
                     </form>
                 </div>
-                <table id="tableStyle">
-                    <tr>
-                        <th name="start_time">Start time</th>
-                        <th name="end_time">End time</th>
-                        <th name="Monday">Monday</th>
-                        <th name="Tuesday">Tuesday</th>
-                        <th name="Wednesday">Wedensday</th>
-                        <th name="Thursday">Thursday</th>
-                        <th name="Friday">Friday</th>
-                    </tr>
-                </table>
             </div>
         </div>
     </div>
