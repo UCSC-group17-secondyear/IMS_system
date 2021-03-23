ed63988a044d66424c3465882f73f8df4b55be1e
milanmalshika <milanmalshika22@gmail.com>
2021-03-24 01:26:49 +0530

    update some functionalities

--- view/admin/aAssignUserRoleV.php
+++ view/admin/aAssignUserRoleV.php
@@ -30,6 +30,7 @@
                             </div>
                             <div class="col-75">
                                 <select name="empid" id="">
+                                    <option value="">Select employee id</option>
                                     <?php echo $_SESSION['userlist'] ?>
                                 </select>
                             </div>
@@ -41,6 +42,7 @@
                             </div>
                             <div class="col-75">
                                 <select name="userRole" id="">
+                                    <option value="">Select role</option>
                                     <?php echo $_SESSION['userroles'] ?>
                                 </select>
                             </div>
