336d2d5c58bcbf6b6de92feee90a1eda1993d302
Chathu Rashmini <64759099+chathuRashmini@users.noreply.github.com>
2021-03-24 15:10:23 +0530

    Updated Manage subjects functionality

---
+++ view/attendanceMaintainer/amInvalidSemesterV.php
@@ -0,0 +1,41 @@
+<?php
+    require '../basic/topnav.php';
+?>
+
+<main>
+    <div class="sansserif">
+        <ul class="breadcrumbs">
+            <li><a href="amHomeV.php">Home</a></li>
+            <li><a href="amDeleteUpdateSubjectSearch.php">Update or Delete Subjects</a></li>
+            <li class="active">Request Denied!</li>
+        </ul>
+
+        <div class="row" style="margin-bottom: 4%;" >
+            <div class="col left20">
+                <?php
+                    require 'amSideNavV.php';
+                ?>
+            </div>
+
+            <div class="col right80">
+                <div class="contentForm">
+                    <div class="row">
+                        <h2>Please note that semester has to be either 1 ('First semester') or 2 ('Second semester'). Thank you!
+                        </h2>
+                    </div>
+
+                    <button class="subbtn">
+                        <a href="amDeleteUpdateSubjectSearch.php">Try again</a> 
+                    </button>
+                    <button class="cancelbtn">
+                        <a href="amHomeV.php">Exit</a> 
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php
+    require '../basic/footer.php';
+?>
---
+++ view/attendanceMaintainer/amSubjectCodeExists.php
@@ -0,0 +1,42 @@
+<?php
+    require '../basic/topnav.php';
+?>
+
+<main>
+    <div class="sansserif">
+        <ul class="breadcrumbs">
+            <li><a href="amHomeV.php">Home</a></li>
+            <li><a href="amDeleteUpdateSubjectSearch.php">Update or Delete Subjects</a></li>
+            <li class="active">Request Denied!</li>
+        </ul>
+
+        <div class="row" style="margin-bottom: 4%;" >
+            <div class="col left20">
+                <?php
+                    require 'amSideNavV.php';
+                ?>
+            </div>
+
+            <div class="col right80">
+                <div class="contentForm">
+                    <div class="row">
+                        <h2>Sorry!
+                            The entered subject-code is already occupied by another subject. Try using a different one. Thank you!
+                        </h2>
+                    </div>
+
+                    <button class="subbtn">
+                        <a href="amDeleteUpdateSubjectSearch.php">Try again</a> 
+                    </button>
+                    <button class="cancelbtn">
+                        <a href="amHomeV.php">Exit</a> 
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php
+    require '../basic/footer.php';
+?>
