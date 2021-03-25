c74139da5182f8876b7f4247709512020d12a614
milanmalshika <milanmalshika22@gmail.com>
2021-03-25 22:53:31 +0530

    update signup function

---
+++ view/admin/aUserNameNotaString.php
@@ -0,0 +1,43 @@
+<?php
+    require '../basic/topnav.php';
+?>
+
+<main>
+    <title>Username is not a string</title>
+
+    <div class="sansserif">
+        <ul class="breadcrumbs">
+            <li><a href="../../controller/basicControllers/homeController.php?user_id=<?php echo $_SESSION['userId'] ?>">Home</a></li>
+            <li class="active">Action Failed!</li>
+        </ul>
+
+        <div class="row">
+            <div class="col left20">
+                <?php
+                    require 'aSideNavV.php';
+                ?>
+            </div>
+
+            <div class="col right80">
+                <div class="contentForm">
+                    <!-- <form action="../../controller/adminControllers/manageUserRoleController.php" method="post"> -->
+                        <div class="row">
+                            <h2>Sorry!
+                                Username is not a string..
+                            </h2>
+                        </div>
+
+                        <button class="cancelbtn">
+                            <a href="../../controller/basicControllers/homeController.php?user_id=<?php echo $_SESSION['userId'] ?>">Home</a>  
+                        </button>
+                        
+                    <!-- </form> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php
+    require '../basic/footer.php';
+?>
