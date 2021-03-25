5fc19c748388d78a05110b1f2af65547c4b2de60
Chathu Rashmini <64759099+chathuRashmini@users.noreply.github.com>
2021-03-25 17:13:27 +0530

    RV - View Batch wise attendance

--- controller/rvControllers/rvViewAttendanceC.php
+++ controller/rvControllers/rvViewAttendanceC.php
@@ -445,5 +445,78 @@
             echo "error";
         }
     }
+
+    elseif (isset($_POST['batchWise-submit'])) {
+        session_start();
+        $degree_id = $_SESSION['degree_id'];
+        $batch_number = $_POST['batch_number'];
+        $subject_name = $_POST['subject_name'];
+        $sessionType = $_POST['sessionType'];
+        $startDate = $_POST['startDate'];
+        $endDate = $_POST['endDate'];
+
+        if ($startDate > $endDate) {
+            /*header('Location:../../view/reportViewer/rvStartEndDateIssueB.php');*/
+            echo "error";
+        }
+        else {
+            $_SESSION['batch_number'] = $batch_number;
+            $_SESSION['subject_name'] = $subject_name;
+            $_SESSION['sessionType'] = $sessionType;
+            $_SESSION['startDate'] = $startDate;
+            $_SESSION['endDate'] = $endDate;
+
+            $result_subject_id = rvModel::getSubjectID($subject_name, $degree_id, $connect);
+            $result1 = mysqli_fetch_assoc($result_subject_id);
+
+            $result_sessionTypeId = rvModel::getSessionTypeID($sessionType, $connect);
+            $result2 = mysqli_fetch_assoc($result_sessionTypeId);
+
+            if ($result1 && $result2) {
+                $subject_id = $result1['subject_id'];
+                $sessionTypeId = $result2['sessionTypeId'];
+
+                $records1 = rvModel::batchAttendance($degree_id, $subject_id, $sessionTypeId, $batch_number, $startDate, $endDate, $connect);
+                $records2 = rvModel::batchAttendancePercentage($degree_id, $subject_id, $sessionTypeId, $batch_number, $startDate, $endDate, $connect);
+                $result_records2 = mysqli_fetch_assoc($records2);
+                $attendPercentage = $result_records2['attendPercentage'];
+                $stdCount = $result_records2['stdCount'];
+                $numOfDays = $result_records2['numOfDays'];
+
+                if ($records1 && $records2) {
+                    if (mysqli_num_rows($records1) == 0) {
+                        /*header('Location:../../view/reportViewer/rvNoBatchAttendance.php');*/
+                        echo "error";
+                    }
+                    else {
+                        $_SESSION['batchWise_attendance'] = '';
+
+                        while ($record = mysqli_fetch_assoc($records1)) {
+                            $std_id = $record['std_id'];
+                            $get_student_index = rvModel::getStdIndex ($std_id, $connect);
+                            $result_student_index = mysqli_fetch_assoc($get_student_index);
+
+                            $_SESSION['batchWise_attendance'] .= "<tr>";
+                            $_SESSION['batchWise_attendance'] .= "<td>{$result_student_index['index_no']}</td>";
+                            $_SESSION['batchWise_attendance'] .= "<td>{$record['attendance']}</td>";
+                            $_SESSION['batchWise_attendance'] .= "</tr>";
+                        }
+                        $_SESSION['attendPercentage'] = $attendPercentage;
+                        $_SESSION['stdCount'] = $stdCount;
+                        $_SESSION['numOfDays'] = $numOfDays;
+                        header('Location:../../view/reportViewer/rvDisplayBatchAttendanceV.php');
+                    }
+                }
+                else {
+                    /*header('Location:../../view/reportViewer/rvNoBatchAttendance.php');*/
+                    echo "error";
+                }
+            }
+            else {
+                /*header('Location:../../view/reportViewer/rvNoSubIDSessionID_Batch.php');*/
+                echo "error";
+            }
+        }
+    }
     
 ?>
--- view/reportViewer/rvSelectSub_B.php
+++ view/reportViewer/rvSelectSub_B.php
@@ -24,61 +24,61 @@
 
                 <div class="contentForm">
                     <form action="../../controller/rvControllers/rvViewAttendanceC.php" method="post">
-                    <div class="row">
-                        <div class="col-25">
-                            <label>Enter Batch Number</label>
+                        <div class="row">
+                            <div class="col-25">
+                                <label>Enter Batch Number</label>
+                            </div>
+                            <div class="col-75">
+                                <input type="number" name="batch_number" placeholder="Batch Number" min="0" required/> <br>
+                            </div>
                         </div>
-                        <div class="col-75">
-                            <input type="number" name="batch_number" placeholder="Batch Number" min="0" required/> <br>
-                        </div>
-                    </div>
 
-                    <div class="row">
-                        <div class="col-25">
-                            <label>Enter Start Date</label>
+                        <div class="row">
+                            <div class="col-25">
+                                <label>Enter Start Date</label>
+                            </div>
+                            <div class="col-75">
+                                <input type="date" name="startDate" placeholder="Start Date" required/> <br>
+                            </div>
                         </div>
-                        <div class="col-75">
-                            <input type="date" name="startDate" placeholder="Start Date" required/> <br>
-                        </div>
-                    </div>
 
-                    <div class="row">
-                        <div class="col-25">
-                            <label>Enter End Date</label>
+                        <div class="row">
+                            <div class="col-25">
+                                <label>Enter End Date</label>
+                            </div>
+                            <div class="col-75">
+                                <input type="date" name="endDate" placeholder="End Date" required/> <br>
+                            </div>
                         </div>
-                        <div class="col-75">
-                            <input type="date" name="endDate" placeholder="End Date" required/> <br>
-                        </div>
-                    </div>
 
-                    <div class="row">
-                        <div class="col-25">
-                            <label>Select Subject</label>
+                        <div class="row">
+                            <div class="col-25">
+                                <label>Select Subject</label>
+                            </div>
+                            <div class="col-75">
+                                <select name="subject_name">
+                                    <?php echo $_SESSION['subject_list']; ?>
+                                </select>
+                            </div>
                         </div>
-                        <div class="col-75">
-                            <select name="subject_name">
-                                <?php echo $_SESSION['subject_list']; ?>
-                            </select>
-                        </div>
-                    </div>
 
-                    <div class="row">
-                        <div class="col-25">
-                            <label>Select Session Type</label>
+                        <div class="row">
+                            <div class="col-25">
+                                <label>Select Session Type</label>
+                            </div>
+                            <div class="col-75">
+                                <select name="sessionType">
+                                    <?php echo $_SESSION['sessionTypes']; ?>
+                                </select>
+                            </div>
                         </div>
-                        <div class="col-75">
-                            <select name="sessionType">
-                                <?php echo $_SESSION['sessionTypes']; ?>
-                            </select>
-                        </div>
-                    </div>
 
-                    <button class="subbtn" type="submit" name="batchWise-submit">Enter
-                    </button>
-                    <button class="cancelbtn" type="submit" name="cancel-submit">
-                        <a href="rvHomeV.php">Cancel</a> 
-                    </button>
-                </form>
+                        <button class="subbtn" type="submit" name="batchWise-submit">Enter
+                        </button>
+                        <button class="cancelbtn" type="submit" name="cancel-submit">
+                            <a href="rvHomeV.php">Cancel</a> 
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
